6537be695443ed63604d0d9fb73314a9aff04e48
ismailj01 <ismailj2800@gmail.com>
2026-08-09 01:13:36 -0400

    re

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://poke-party.vercel.app"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
